df2a4db2147027455c7164e49f80fce3a2d676ab
philippeducasse <ducassephi@hotmail.fr>
2023-03-06 20:22:07 +0000

    Created first pokemon array
    If applied, this commit will create the first pokemon array in JS.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let pokemonList=[
+    {name:'Bulbasaur', height: 4, type: ['grass','poison']},
+    {name:'Charmander', height:12, type: ['fire','fight']},
+    {name:'Squirtle', hieght:9, type: ['water', 'ice']},
+    {name:'Hypno', height: 10, type: ['psychic','dark']}
+]
