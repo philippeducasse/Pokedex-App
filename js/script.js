fc06bc59353c8e6cae260c02d028736892933f7e
philippeducasse <ducassephi@hotmail.fr>
2023-03-08 16:01:11 +0000

    New for loop added
    If implemented, this commit creates a for loop which writes th pokemons name and its height on the webpage.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 let pokemonList=[
-    {name:'Bulbasaur', height: 4, type: ['grass','poison']},
-    {name:'Charmander', height:12, type: ['fire','fight']},
-    {name:'Squirtle', hieght:9, type: ['water', 'ice']},
-    {name:'Hypno', height: 10, type: ['psychic','dark']}
+    {
+        name:'Bulbasaur',
+        height: 4,
+        type: ['grass','poison']
+    },
+    {
+        name:'Charmander',
+        height:12,
+        type: ['fire','fight']
+    },
+    {
+        name:'Squirtle',
+        height:9,
+        type: ['water', 'ice']
+    },
+    {
+        name:'Hypno',
+        height: 10,
+        type: ['psychic','dark']
+    }
 ]
+for (let i=0; i<pokemonList.length; i++){
+    document.write(pokemonList[i].name   +  '  (height:  '  + pokemonList[i].height  + ')  ')
+    if (pokemonList[i].height>10){
+        document.write(" - Wow, that´s big! <br>") 
+    } else {
+        document.write("<br>")
+    }
+}
