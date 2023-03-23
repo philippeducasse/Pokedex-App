460881fffe635377f0f816016f8d48e64894df77
philippeducasse <ducassephi@hotmail.fr>
2023-03-23 21:47:53 +0100

    modal added
    This commit ads a modal which displays pokemon info when the button is clicked

--- js/script.js
+++ js/script.js
@@ -5,12 +5,15 @@ let pokemonRepository = (function(){
 
     let apiUrl= 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+    let modalContainer= document.querySelector('#modal-container');
+
     function getAll () {
         return pokemonList
         };
     function add(pokemon){
         pokemonList.push(pokemon);
         } 
+
     function addListItem(pokemon){
     
         let pokemonList = document.querySelector('.pokemon-list');
@@ -23,13 +26,57 @@ let pokemonRepository = (function(){
         buttonClick(button,pokemon);
     }
 
+    function showModal(title,text,image) {
+
+        modalContainer.innerHTML= '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'close';
+        closeButtonElement.addEventListener('click', hideDetails);
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = 'weight: ' + text;
+        
+        let contentImage = document.createElement('img');
+        contentImage.src = image;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(contentImage);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+
+    };
+
+    window.addEventListener('keydown', (e) => {
+        
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+          hideDetails();  
+        }
+      });
+
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
-          console.log(pokemon);
+
+            showModal(pokemon.name, pokemon.weight, pokemon.imageURL);
         });
-      }
+    }
+
+        function hideDetails (){
+            modalContainer.classList.remove('is-visible');
+        }
+
+
 // bonus task
      function buttonClick(button,pokemon){
+        
         button.addEventListener('click',function(){showDetails(pokemon)})
      }
 
@@ -82,7 +129,7 @@ let loadingMessage = document.querySelector('.loading-message');
         loadingMessage.classList.add('invisible');
      }
 
-        
+    // these are methods that can be called outside IIFE    
     return { 
         add: add,
         getAll: getAll, 
@@ -102,42 +149,5 @@ pokemonRepository.loadList().then(function(){
     });
 }); 
 
-   // ATTEMPT AT BONUS: DOESNT WORK "POKEMON IS NOT DEFINED"
-     
-//    if (
-//     typeof pokemon === 'object' &
-//     typeof pokemon.name === 'string' &
-//     typeof pokemon.weight === 'number' &
-//     Array.isArray(pokemon.type)
-//     ) {
-//         pokemonList.push(pokemon);
-//     } else {
-//         console.log('Invalid') 
-//     };
-        // dont get this bit
-// forEach loop
 
-// pokemonRepository.getAll().forEach(function(pokemon) {
-    
-//     document.write(pokemon.name + " (weight: " + pokemon.weight + "), ");
-//         }); 
-// how would I add a pokemon using the add function?
-
-// ATTEMPT AT LAST BONUS PART 
-
-
-// let result = pokemonRepository.filter (pokemon => pokemon.name === pokemon);
-
-// console.log (result)
-
-// pokemonRepository.getAll().forEach(function(pokemon) {
-//     let newList = document.querySelector('.pokemon-list');
-//     let listItem = document.createElement('li');
-//         newList.appendChild(listItem);
-//     let button = document.createElement('button');
-//         button.innerText = pokemon.name;
-//         button.classList.add = 'new-pokemon';
-//     listItem.appendChild(button);
-
-//             }); 
 
